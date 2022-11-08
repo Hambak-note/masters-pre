87095cfb9fa9cf2688f056f4ddbca28a5f956beb
Hambak-note <dudghks5722@gmail.com>
2022-11-08 17:30:18 +0900

    Create 코드스쿼드 프리코스 2주차 dya2 추가문제 2번 솔루션 코드

---
+++ mission/src/week2/day2/question2/Main.java
@@ -0,0 +1,55 @@
+package week2.day2.question2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        MonthSchedule monthSchedule = new MonthSchedule();
+        while(!run(monthSchedule)){
+
+        }
+    }
+
+    static boolean run(MonthSchedule monthSchedule) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int inputMenu;
+        System.out.println("이번 달 스케줄 관리 프로그램");
+        System.out.print("할일(입력:1, 보기:2, 끝내기:3) >> ");
+        inputMenu = Integer.parseInt(br.readLine());
+
+        boolean isNormalEnd = false;
+
+        int day;
+        String work;
+        switch (inputMenu){
+            case 1 :
+                System.out.print("날짜(1~30)?");
+                day = Integer.parseInt(br.readLine());
+                System.out.print("할일(빈칸없이입력)?");
+                work = br.readLine();
+                System.out.println();
+                monthSchedule.input(day, work);
+                break;
+            case 2:
+                System.out.print("날짜(1~30)?");
+                day = Integer.parseInt(br.readLine());
+                System.out.println(day + "의 할일은 " + monthSchedule.hasWork(day));
+                System.out.println();
+                break;
+            case 3:
+                System.out.println("프로그램을 종료 합니다.");
+                isNormalEnd = true;
+                break;
+            default:
+                System.out.println("제대로 된 입력을 입력해주세요");
+                isNormalEnd = false;
+                break;
+        }
+
+        return isNormalEnd;
+    }
+}
---
+++ mission/src/week2/day2/question2/MonthSchedule.java
@@ -0,0 +1,22 @@
+package week2.day2.question2;
+
+public class MonthSchedule {
+
+    private Day[] days;
+
+    public MonthSchedule() {
+        days = new Day[31];
+        for(int i = 1; i < days.length; i++){
+            days[i] = new Day();
+        }
+    }
+
+    public void input(int day, String work){
+        days[day].saveTodayWork(work);
+        System.out.println(days[day].hasWork());
+    }
+
+    public String hasWork(int day){
+        return days[day].hasWork();
+    }
+}
