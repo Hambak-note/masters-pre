427844d61c57f2adda6e941f4ec02493195de975
Hambak-note <dudghks5722@gmail.com>
2022-11-09 15:20:30 +0900

    Create 2주차 알고리즘 2016년 문제 솔루션 코드

---
+++ mission/src/week2/day3/Year2016.java
@@ -0,0 +1,25 @@
+package week2.day3;
+
+public class Year2016 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public String solution(int a, int b) {
+            String answer = "";
+            String[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+            int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int allDate = 0;
+            for (int i = 0; i < a - 1; i++) {
+                allDate += date[i];
+            }
+
+            allDate += (b - 1);
+            answer = day[allDate % 7];
+
+            return answer;
+        }
+    }
+}
