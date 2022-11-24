1be5d0290000e88a2bb1ff3c14b03e7b216d0a2b
Hambak-note <dudghks5722@gmail.com>
2022-11-24 09:17:10 +0900

    feat: week4 day3 알고리즘 문제 3번 솔루션 코드

---
+++ mission/src/week4/day3/Question3.java
@@ -0,0 +1,29 @@
+package week4.day3;
+
+public class Question3 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public boolean isPalindrome(int x) {
+
+            String inputStr = String.valueOf(x);
+            if(inputStr.charAt(0) == '-'){
+                return false;
+            }
+
+            StringBuilder reverse = new StringBuilder();
+            for (int i = inputStr.length()-1; i >=0; i--) {
+                reverse.append(inputStr.charAt(i));
+            }
+            String reverseStr = reverse.toString();
+            if(reverseStr.equals(inputStr)){
+                return true;
+            }
+
+            return false;
+    }
+}
+
